7f28b3cb1ef146f741ae7c54910de5bd5c452562
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-05-18 22:24:02 -0300

    Implementado o CRUD para as memories

--- prisma/migrations/20230518211835_create_memories/migration.sql
+++ prisma/migrations/20230518211835_create_memories/migration.sql
@@ -12,7 +12,7 @@ CREATE TABLE "Memory" (
     "userId" TEXT NOT NULL,
     "coverUrl" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "isPublick" BOOLEAN NOT NULL DEFAULT false,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "Memory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
