be44c7a9d49d88887f9065e7bb20c6430a44aa6c
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-05-19 18:39:06 -0300

    implementada autentaticação Oauth com github

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
 /* eslint-disable prettier/prettier */
+import 'dotenv/config'
+
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true, // Todas URLs de front-end poderão acessar este back-end
+  // origin: ['http://site.com.br/', 'http://localhost:3000'] // Forma de selecionar URLs específicas
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server runnign on https://localhost:3333')
